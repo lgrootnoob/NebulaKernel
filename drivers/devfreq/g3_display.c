b182282bcc54ebad32d2aeac0703e54c1cd72133
lgrootnoob <jaclane299@gmail.com>
2016-03-17 17:12:23 -0600

    38 fps

--- drivers/devfreq/g3_display.c
+++ drivers/devfreq/g3_display.c
@@ -70,8 +70,8 @@ struct platform_driver g3_display_driver = {
 
 static int g3_cur_level = _LV_END_ - 1;
 struct display_opp_table g3_display_opp_table[] = {
-	{LV_0, 50, 0},
-	{LV_1, 60, 0},
+	{LV_0, 38, 0},
+	{LV_1, 38, 0},
 };
 
 static int g3_get_cur_freq(struct device *dev, unsigned long *freq){
@@ -82,7 +82,7 @@ static int g3_get_cur_freq(struct device *dev, unsigned long *freq){
 
 // Profile Display OPP
 struct devfreq_dev_profile g3_display_profile = {
-	.initial_freq = 60,
+	.initial_freq = 38,
 	.polling_ms = 0,
 	.get_dev_status = g3_display_get_dev_status,
 	.target = g3_display_profile_target,
@@ -262,7 +262,7 @@ int g3_display_send_event_to_mdss_display(unsigned long val, void *v){
 		ret = mdss_mdp_ctl_update_fps(mdp5_data->ctl, wdfps);
 	} else if (wdfps > 60) {
 		pr_err("Unsupported FPS. Configuring to max_fps = 60\n");
-		wdfps = 60;
+		wdfps = 38;
 		ret = mdss_mdp_ctl_update_fps(mdp5_data->ctl, wdfps);
 	} else {
 		ret = mdss_mdp_ctl_update_fps(mdp5_data->ctl, wdfps);
